4cb7e2a35ec84b317f6e5e3c5d96bb854acce219
jerryjohns <jerry.johns@student.moringaschool.com>
2022-08-07 00:07:57 +0300

    add

--- src/Nav.js
+++ src/Nav.js
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import './App.css'
-// import {Link} from "react-router-dom";
+import React from 'react';
+import './App.css'
+import {Link} from "react-router-dom";
 
-// const Nav = () => {
-//   return (
-//     <nav className='NavBar'>
-//           <ul>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-//             <li>
-//               <Link to="/about">About</Link>
-//             </li>
-//             <li>
-//               <Link to="/contact">Contact</Link>
-//             </li>
-//             <li>
-//               <Link to="/review">Review</Link>
-//             </li>
+const Nav = () => {
+  return (
+    <nav className='NavBar'>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/review">Review</Link>
+            </li>
           
-//             </ul>
-//         </nav>
-//   )
-// }
+            </ul>
+        </nav>
+  )
+}
 
-// export default Nav
+export default Nav
