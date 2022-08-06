6ecc77c43433c02bf012cc56d3ba6e77b2aecca4
jerryjohns <jerry.johns@student.moringaschool.com>
2022-08-07 00:06:03 +0300

    add

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,10 @@
 
-// import React from 'react'
+import React from 'react'
 
-// const Home = () => {
-//   return (
-//     <div>Home</div>
-//   )
-// }
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
 
-// export default Home
+export default Home
